6c5d4890bec5e2dd0cb3301fe9bd0e882a5ca952
Feliphe Silva <feliphevs90@outlook.com>
2021-12-28 11:59:22 -0300

    router implementado

--- src/App.vue
+++ src/App.vue
@@ -17,29 +17,7 @@
 
     <!-- Login -->
     <v-main>
-      <v-card width="500" class="mx-auto mt-5">
-        <v-card-title><h1 class="display-1">Login</h1></v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                label="Username"
-                prepend-icon="mdi-account-circle"
-              ></v-text-field>
-              <v-text-field 
-                label="Password"
-                prepend-icon="mdi-lock"
-                :append-icon="exibir ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="exibir ? 'text' : 'password'"
-                @click:append="showHide"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Register</v-btn>
-        </v-card-actions>
-      </v-card>
+     <router-view></router-view>
     </v-main>
     
     <v-footer
@@ -83,14 +61,11 @@ export default {
   },
 
   data: () => ({
-    exibir: false,
     links: ['Home', 'About Us']
   }),
 
   methods:{
-    showHide(){
-      this.exibir = !this.exibir;
-    },
+    
   },
 };
 </script>
